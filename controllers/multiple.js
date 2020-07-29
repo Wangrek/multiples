9f0ba264b1ea4f9fcee630f7941486543ffc14a2
Wangrek <wangrek@gmail.com>
2020-07-29 15:28:12 -0500

    Refactoring

--- controllers/multiple.js
+++ controllers/multiple.js
@@ -2,41 +2,22 @@
 
 function getData(req, res) {
 	var output = [];
-	for (var i=1; i<=100; i++ ) {
-    switch(multiple(i,3) && multiple(i,5)) {
-      case true:
-        console.log('Musical');
-        output.push('Musical');
-        break;
-      case false:
-        switch(multiple(i,5)) {
-          case true:
-            console.log('TI');
-            output.push('TI');
-            break;
-          case false:
-            switch(multiple(i,3)) {
-              case true:
-                console.log('Music');
-                output.push('Music');
-                break;
-              default:
-                console.log(i);
-                output.push(i);
-                break;
-            }    
-        }
-    }
+
+  for (var i=1; i<=100; i++ ) {
+    output.push(multiple(i));
   }
   return res.status(200).send({output});
 }
 
-function multiple(value, multiple) {
-    var resto = value % multiple;
-    if (resto === 0) {
-      return true;
-    } 
-    return false;
+function multiple(i) {
+    var words = ["Musical", "TI", "Music"];
+    var index = Math.min(i % 3, 1) + Math.min(i % 5, 1) * 2;
+    if (index < words.length) {
+      console.log(words[index]);
+      return words[index];
+    }  
+    console.log(i);
+    return i;
 }
 
 module.exports = {
